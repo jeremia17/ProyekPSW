08b8c7fe769afaf9b673cc874a832e6aeaca525e
jeremia17 <rinojeremia17@gmail.com>
2024-06-07 11:40:19 +0700

    Commit
    Tambahan

---
+++ resources/views/components/application-mark.blade.php
@@ -0,0 +1,4 @@
+<div class="inline-flex items-center">
+  <img src="{{ asset('images/logooo.jpeg') }}" alt="" class="h-16 w-16">
+  <h1 class="text-3xl font-bold text-yellow-500"> POLONIA BADMINTON </h1>
+</div>
